faf55a87f739cbdcb02d2b0b0af38ac15584c3d9
CHENTing <krystal_0902@icloud.com>
2019-01-18 19:27:27 +0100

    succeed the xml parser function

---
+++ src/fr/insa/soa/bean/Light.java
@@ -0,0 +1,28 @@
+package fr.insa.soa.bean;
+
+public class Light {
+	
+	String status;
+	String name;
+	int id;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
